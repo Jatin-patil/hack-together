b8770122ce5beb3540513c86013af7ffb9ea0935
Jatin-patil <56484019+Jatin-patil@users.noreply.github.com>
2023-03-11 05:09:30 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,86 @@
+﻿Console.WriteLine("welcome to the Teams Meeting Scheuler App!!!\n");
+
+var settings = Settings.LoadSettings();
+
+// Initialize Graph
+InitializeGraph(settings);
+
+// Greet the user by name
+await GreetUserAsync();
+
+int choice = -1;
+
+while (choice != 0)
+{
+    Console.WriteLine("Please choose one of the following options:");
+    Console.WriteLine("0. Exit");
+    Console.WriteLine("1. Create Teams Meet");
+
+    try
+    {
+        choice = int.Parse(Console.ReadLine() ?? string.Empty);
+    }
+    catch (System.FormatException)
+    {
+        // Set to invalid value
+        choice = -1;
+    }
+
+    switch (choice)
+    {
+        case 0:
+            // Exit the program
+            Console.WriteLine("Goodbye...");
+            break;
+        case 1:
+            // Display access token
+            await CreateTeamsMeet();
+            break;
+        default:
+            Console.WriteLine("Invalid choice! Please try again.");
+            break;
+    }
+}
+
+void InitializeGraph(Settings settings)
+{
+    GraphHelper.InitializeGraphForUserAuth(settings,
+        (info, cancel) =>
+        {
+            // Display the device code message to
+            // the user. This tells them
+            // where to go to sign in and provides the
+            // code to use.
+            Console.WriteLine(info.Message);
+            return Task.FromResult(0);
+        });
+}
+
+async Task GreetUserAsync()
+{
+    try
+    {
+        var user = await GraphHelper.GetUserAsync();
+        Console.WriteLine($"Hello, {user?.DisplayName}!");
+        // For Work/school accounts, email is in Mail property
+        // Personal accounts, email is in UserPrincipalName
+        Console.WriteLine($"Email: {user?.Mail ?? user?.UserPrincipalName ?? ""}");
+    }
+    catch (Exception ex)
+    {
+        Console.WriteLine($"Error getting user: {ex.Message}");
+    }
+}
+
+async Task CreateTeamsMeet()
+{
+    try
+    {
+        var meetingURL = await GraphHelper.CreateTeamsMeet();
+        Console.WriteLine($"Teams Meeting has been scheduled succesfully, Please join via URL : {meetingURL}");
+    }
+    catch (Exception ex)
+    {
+        Console.WriteLine($"Error while creating a meet: {ex.Message}");
+    }
+}
